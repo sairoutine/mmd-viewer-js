fd403dd4a13da1578c4007e9eeb9015d4a8fdb9f
sairoutine <sairoutine@gmail.com>
2016-10-28 10:55:31 +0900

    refactor Inherit

--- src/js/Inherit.js
+++ src/js/Inherit.js
@@ -1,23 +1,28 @@
+'use strict';
 /**
  *
  */
-function __inherit( child, parent ) {
+
+var Util = {};
+
+Util.__inherit = function( child, parent ) {
   var getPrototype = function( p ) {
     if( Object.create ) {
       return Object.create( p ) ;
     }
-    function f( ) { } ;
-    f.prototype = p ;
-    return new f( ) ;
+    function F( ) { }
+    F.prototype = p ;
+    return new F( ) ;
   } ;
   child.prototype = getPrototype( parent.prototype ) ;
   child.prototype.constructor = child ;
-}
+};
 
 
-function __copyParentMethod(child, parent, methodName) {
+Util.__copyParentMethod = function (child, parent, methodName) {
   var parentName = parent.name;
   var name = parentName + '_' + 
-               ((methodName[0] == '_') ? methodName.slice(1) : methodName);
+               ((methodName[0] === '_') ? methodName.slice(1) : methodName);
   child.prototype[name] = parent.prototype[methodName];
-}
+};
+module.exports = Util;
